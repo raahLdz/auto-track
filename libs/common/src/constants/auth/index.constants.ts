5aaaa43ee8696f851cfb19190c5a7d48692c3e65
Israel Ledezma <israeledezmag@gmail.com>
2026-03-13 12:54:50 -0600

    first commit

---
+++ libs/common/src/constants/auth/index.constants.ts
@@ -0,0 +1,20 @@
+export const AUTH_EVENTS = {
+    CLIENTS: {
+        LOGIN: 'auth.clients.login',
+        LOGOUT: 'auth.clients.logout',
+        REFRESH: 'auth.clients.refresh',
+        REGISTER: 'auth.clients.register',
+    },
+    WORK_SHOP: {
+        LOGIN: 'auth.workshop.login',
+        LOGOUT: 'auth.workshop.logout',
+        REFRESH: 'auth.workshop.refresh',
+        REGISTER: 'auth.workshop.register',
+    },
+    ADMIN: {
+        LOGIN: 'auth.admin.login',
+        LOGOUT: 'auth.admin.logout',
+        REFRESH: 'auth.admin.refresh',
+        REGISTER: 'auth.admin.register',
+    },
+};
